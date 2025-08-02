79f00d35f33602f089cb4b01ca703a3261be4976
MysteryCodes <39903046+DKTJONATHAN@users.noreply.github.com>
2025-08-02 17:37:48 +0300

    Update [slug].js

--- pages/articles/[slug].js
+++ pages/articles/[slug].js
@@ -1,24 +1,33 @@
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-
-export default function Article() {
-  const { slug } = useRouter().query;
-  const [content, setContent] = useState("");
-
-  useEffect(() => {
-    if (slug) {
-      fetch(
-        `https://raw.githubusercontent.com/${process.env.GITHUB_OWNER}/${process.env.GITHUB_REPO}/main/content/articles/${slug}.html`,
-        {
-          headers: {
-            Authorization: `token ${process.env.GITHUB_TOKEN}`,
-          },
-        }
-      )
-        .then(res => res.text())
-        .then(data => setContent(data));
-    }
-  }, [slug]);
-
+export default function Article({ content }) {
   return <div dangerouslySetInnerHTML={{ __html: content }} />;
+}
+
+export async function getStaticPaths() {
+  const response = await fetch(
+    `https://api.github.com/repos/${process.env.GITHUB_OWNER}/${process.env.GITHUB_REPO}/contents/content/articles`,
+    {
+      headers: {
+        Authorization: `token ${process.env.GITHUB_TOKEN}`,
+      },
+    }
+  );
+  const files = await response.json();
+  const paths = files.map(file => ({
+    params: { slug: file.name.replace(".html", "") },
+  }));
+
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }) {
+  const content = await fetch(
+    `https://raw.githubusercontent.com/${process.env.GITHUB_OWNER}/${process.env.GITHUB_REPO}/main/content/articles/${params.slug}.html`,
+    {
+      headers: {
+        Authorization: `token ${process.env.GITHUB_TOKEN}`,
+      },
+    }
+  ).then(res => res.text());
+
+  return { props: { content } };
 }
