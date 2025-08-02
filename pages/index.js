3cb80a679af18fca656301ddc76d12f248e5c0d5
MysteryCodes <39903046+DKTJONATHAN@users.noreply.github.com>
2025-08-03 00:46:37 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
   useEffect(() => {
     setLoading(true);
-    fetch("/api/list-posts")
+    fetch("/api/list-posts-temp")
       .then(res => res.json())
       .then(data => {
         if (data.error) {
