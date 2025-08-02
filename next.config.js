208d509add62b44bc1c8a92fb6c6e63ec63096ce
MysteryCodes <39903046+DKTJONATHAN@users.noreply.github.com>
2025-08-02 18:17:34 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Enables static export for GitHub Pages compatibility
+  output: "export", // Enables static export
   reactStrictMode: true,
   trailingSlash: true, // Ensures URLs work with static export
+  env: {
+    GITHUB_OWNER: process.env.GITHUB_OWNER,
+    GITHUB_REPO: process.env.GITHUB_REPO,
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN,
+  },
 };
 
 export default nextConfig;
