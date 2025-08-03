d73b807386e77b97774379228ab41ee0384de3e7
MysteryCodes <39903046+DKTJONATHAN@users.noreply.github.com>
2025-08-03 10:07:47 +0300

    Update [category].js

--- pages/category/[category].js
+++ pages/category/[category].js
@@ -3,6 +3,10 @@ import Header from "../../components/Header";
 export default function CategoryPage({ category }) {
   return (
     <>
+      <Head>
+        <title>{category} - Jonathan Mwaniki News</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+      </Head>
       <Header />
       <link rel="stylesheet" href="/styles.css" />
       <div className="container">
@@ -10,18 +14,20 @@ export default function CategoryPage({ category }) {
         <div className="posts" />
       </div>
       <nav className="bottom-nav">
-        <a href="/">🏠</a>
-        <a href="/category/News">📰</a>
-        <a href="/category/Business">💼</a>
-        <a href="/category/Tech">💻</a>
-        <a href="/category/Sports">⚽</a>
-        <a href="/category/Entertainment">🎬</a>
-        <a href="/category/Opinions">💭</a>
+        <a href="/"><i className="fas fa-home"></i></a>
+        <a href="/category/News"><i className="fas fa-newspaper"></i></a>
+        <a href="/category/Business"><i className="fas fa-briefcase"></i></a>
+        <a href="/category/Tech"><i className="fas fa-laptop"></i></a>
+        <a href="/category/Sports"><i className="fas fa-futbol"></i></a>
+        <a href="/category/Entertainment"><i className="fas fa-film"></i></a>
+        <a href="/category/Opinions"><i className="fas fa-comment"></i></a>
       </nav>
       <script src="/news-script.js" />
     </>
   );
 }
+
+import { Head } from "next/document";
 
 export async function getStaticPaths() {
   return {
