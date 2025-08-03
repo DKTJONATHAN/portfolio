4fd25b6fa15480da0f4b1384d0bafc6f7d7219bf
MysteryCodes <39903046+DKTJONATHAN@users.noreply.github.com>
2025-08-03 11:04:03 +0300

    Create components/Header.js

---
+++ news-website/components/Header.js
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import styles from '../styles/Header.module.css';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <Link href="/" className={styles.logo}>
+          <h2>Jonathan Mwaniki News</h2>
+        </Link>
+        <nav className={styles.nav}>
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+          <Link href="/contact">Contact</Link>
+        </nav>
+      </div>
+    </header>
+  );
+}
