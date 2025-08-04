e7e03a74fa15719f19e64786ff3377129dbead72
MysteryCodes <39903046+DKTJONATHAN@users.noreply.github.com>
2025-08-04 11:36:20 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,11 +11,7 @@ const nextConfig = {
       }
     ]
   },
-  env: {
-    GITHUB_TOKEN: process.env.GITHUB_TOKEN,
-    GITHUB_OWNER: process.env.GITHUB_OWNER,
-    GITHUB_REPO: process.env.GITHUB_REPO
-  }
+  
 }
 
 module.exports = nextConfig
